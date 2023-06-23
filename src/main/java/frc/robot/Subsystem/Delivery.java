8ad7edef1b830eb8de9bf0a8416f1e9b77101b5b
Howard Walz <howardwalz@gmail.com>
2023-06-23 08:59:46 -0700

    Delivery Subsystem added, need to do config

---
+++ src/main/java/frc/robot/Subsystem/Delivery.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystem;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.MotorController.SOTA_MotorController;
+
+public class Delivery extends SubsystemBase {
+
+  private SOTA_MotorController motor;
+
+  private DigitalInput topSensor;
+  private DigitalInput bottomSensor;
+
+  private double INTAKE_SPEED; //TODO: add config
+  private double OUTTAKE_SPEED;
+  private double SHOOT_SPEED;
+
+  /** Creates a new Delivery. */
+  public Delivery(SOTA_MotorController motor, DigitalInput topSensor, DigitalInput bottomSensor) {
+    this.motor = motor;
+    this.topSensor = topSensor;
+    this.bottomSensor = bottomSensor;
+  }
+
+  /**
+   * Runs delivery towards flywheel for intake
+   */
+  public void intake(){
+    motor.set(INTAKE_SPEED);
+  }
+
+  /**
+   * Runs delivery away from flywheel
+   */
+  public void outTake(){
+    motor.set(OUTTAKE_SPEED);
+  }
+
+  /**
+   * Runs delivery towards flywheel for shooting
+   */
+  public void shoot(){
+    motor.set(SHOOT_SPEED);
+  }
+
+  /**
+   * 
+   *  @return State of top sensor
+   */
+  public boolean getTopSensor(){
+    return topSensor.get();
+  }
+
+  /**
+   * 
+   * @return State of bottom sensor
+   */
+  public boolean getBottomSensor(){
+    return bottomSensor.get();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
