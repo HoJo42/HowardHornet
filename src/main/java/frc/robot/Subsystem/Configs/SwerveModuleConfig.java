0b7a664037fee8bb0089a98247b0493f95a39174
HoJo42 <howardwalz@gmail.com>
2023-07-06 19:25:18 -0700

    added jsons, maxwheelspeed for seprate gears

--- src/main/java/frc/robot/Subsystem/Configs/SwerveModuleConfig.java
+++ src/main/java/frc/robot/Subsystem/Configs/SwerveModuleConfig.java
@@ -3,9 +3,11 @@ package frc.robot.Subsystem.Configs;
 public class SwerveModuleConfig {
     private double angleCountsPerRevolution;
     private double speedCountsPerRevolution;
-    private double wheelCircumference;
+    private double wheelDiameter;
     private double lowGearRatio;
     private double highGearRatio;
+    private double lowGearMaxSpeed;
+    private double highGearMaxSpeed;
 
     private double angleP;
     private double angleI;
@@ -25,151 +27,88 @@ public class SwerveModuleConfig {
     private double speedS;
     private double speedV;
 
+    public double getLowGearMaxSpeed() {
+        return this.lowGearMaxSpeed;
+    }
+
+    public double getHighGearMaxSpeed() {
+        return this.highGearMaxSpeed;
+    }
+
     public double getSpeedCountsPerRevolution() {
         return this.speedCountsPerRevolution;
     }
 
-    public void setSpeedCountsPerRevolution(double speedCountsPerRevolution) {
-        this.speedCountsPerRevolution = speedCountsPerRevolution;
+    public double getWheelDiameter() {
+        return this.wheelDiameter;
     }
 
     public double getWheelCircumference() {
-        return this.wheelCircumference;
-    }
-
-    public void setWheelCircumference(double wheelCircumference) {
-        this.wheelCircumference = wheelCircumference;
+        return wheelDiameter * Math.PI;
     }
 
     public double getLowGearRatio() {
         return this.lowGearRatio;
     }
 
-    public void setLowGearRatio(double lowGearRatio) {
-        this.lowGearRatio = lowGearRatio;
-    }
-
     public double getHighGearRatio() {
         return this.highGearRatio;
     }
 
-    public void setHighGearRatio(double highGearRatio) {
-        this.highGearRatio = highGearRatio;
-    }
-
-    
     public double getSpeedS() {
         return this.speedS;
-    }
-
-    public void setSpeedS(double speedS) {
-        this.speedS = speedS;
     }
 
     public double getSpeedV() {
         return this.speedV;
     }
 
-    public void setSpeedV(double speedV) {
-        this.speedV = speedV;
-    }
-
-    
-
     public double getAngleS() {
         return this.angleS;
-    }
-
-    public void setAngleS(double angleS) {
-        this.angleS = angleS;
     }
 
     public double getAngleV() {
         return this.angleV;
     }
 
-    public void setAngleV(double angleV) {
-        this.angleV = angleV;
-    }
-
     public double getAngleP() {
         return this.angleP;
-    }
-
-    public void setAngleP(double angleP) {
-        this.angleP = angleP;
     }
 
     public double getAngleI() {
         return this.angleI;
     }
 
-    public void setAngleI(double angleI) {
-        this.angleI = angleI;
-    }
-
     public double getAngleD() {
         return this.angleD;
-    }
-
-    public void setAngleD(double angleD) {
-        this.angleD = angleD;
     }
 
     public double getAngleMaxVelocity() {
         return this.angleMaxVelocity;
     }
 
-    public void setAngleMaxVelocity(double angleMaxVelocity) {
-        this.angleMaxVelocity = angleMaxVelocity;
-    }
-
     public double getAngleMaxAcceleration() {
         return this.angleMaxAcceleration;
-    }
-
-    public void setAngleMaxAcceleration(double angleMaxAcceleration) {
-        this.angleMaxAcceleration = angleMaxAcceleration;
     }
 
     public double getSpeedP() {
         return this.speedP;
     }
 
-    public void setSpeedP(double speedP) {
-        this.speedP = speedP;
-    }
-
     public double getSpeedI() {
         return this.speedI;
-    }
-
-    public void setSpeedI(double speedI) {
-        this.speedI = speedI;
     }
 
     public double getSpeedD() {
         return this.speedD;
     }
 
-    public void setSpeedD(double speedD) {
-        this.speedD = speedD;
-    }
-
     public double getSpeedMaxVelocity() {
         return this.speedMaxVelocity;
     }
 
-    public void setSpeedMaxVelocity(double speedMaxVelocity) {
-        this.speedMaxVelocity = speedMaxVelocity;
-    }
-
     public double getSpeedMaxAcceleration() {
         return this.speedMaxAcceleration;
-    }
-
-    public void setSpeedMaxAcceleration(double speedMaxAcceleration) {
-        this.speedMaxAcceleration = speedMaxAcceleration;
     }
 
     public double getAngleCountsPerRevolution() {
